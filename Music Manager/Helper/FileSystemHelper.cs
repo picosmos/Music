e69ef11c976ea2b7db20cc5514c32ebc0c4f5521
Tom Lambert <tom.lambert.13@live.com>
2016-04-20 23:08:02 +0200

    Expanded FileSystemHelper

---
+++ Music Manager/Helper/FileSystemHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Koopakiller.Apps.MusicManager.Helper
+{
+    public static class FileSystemHelper
+    {
+        public static IReadOnlyCollection<string> SupportedMusicFileExtensions => new[] { ".mp3", ".wav", ".m4a", ".flac" };
+
+        public static IEnumerable<string> GetFilesFromDirectory(string folder, IReadOnlyCollection<string> extensions)
+        {
+            foreach (var file in Directory.GetFiles(folder))
+            {
+                var fi = new FileInfo(file);
+                if (extensions.Any(x => fi.Extension.Equals(x, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    yield return file;
+                }
+            }
+
+            foreach (var file in Directory.GetDirectories(folder).SelectMany(x => GetFilesFromDirectory(x, extensions)))
+            {
+                yield return file;
+            }
+        }
+    }
+}
