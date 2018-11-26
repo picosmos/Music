3a4096423a539cb635c243d5083070d42366e8d9
Tom Lambert <koopakiller@live.de>
2018-11-26 09:20:29 +0100

    Persistent Library Path Store

--- src/app/components/Library.ts
+++ src/app/components/Library.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { MusicViewModel } from "../models/MusicViewModel";
+import { LibraryService } from "../services/LibraryService";
 
 @Component({
   selector: "library",
   templateUrl: "./Library.web.html",
   styleUrls: ["./Library.web.less"]
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit {
   constructor(
+    private _libraryService: LibraryService
   ) {
   }
 
   public paths: string[] = [];
 
   public addPath(path: string) {
+    if(!path){
+      return;
+    }
+    
+    this._libraryService.addPath(path);
     this.paths.push(path);
+  }
+
+
+  public async ngOnInit() {
+    this.paths = await this._libraryService.getPaths();
   }
 }
--- src/app/services/LibraryService.ts
+++ src/app/services/LibraryService.ts
@@ -1,11 +1,40 @@
-import { Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
+import * as Datastore from "nedb";
 
 @Injectable()
 export class LibraryService {
-
     constructor() {
+        this._db = new Datastore({ filename: 'datafile.nedb', autoload: true });
     }
 
-    
+    private _db: Datastore;
 
+    public addPath(path: string): void {
+        this._db.insert(new PathModel(path));
+    }
+
+    public async getPaths(): Promise<string[]> {
+        return new Promise<string[]>(resolve => {
+            this._db.find({ type: "path" }, (err, docs: PathModel[]) => {
+                let result = [];
+                for (let path of docs) {
+                    result.push(path.path);
+                }
+                resolve(result);
+            });
+        });
+    }
+}
+
+export interface IModel {
+    type: string;
+}
+
+export class PathModel implements IModel {
+    public get type(): string { return "path" }
+
+    public constructor(
+        public path: string
+    ) {
+    }
 }
